ba3b32f55e628568bd0d56253095c70047618323
Shipra1111 <shipra.sia.10@gmail.com>
2024-10-17 22:17:21 +0800

    Deleted files

--- LearningSelenium/FirstAutomation.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-#driver = webdriver.Chrome()
-driver = webdriver.Chrome()
-driver.get("https://www.google.com")
-print(driver.title)
-driver.maximize_window()
-driver.close()
